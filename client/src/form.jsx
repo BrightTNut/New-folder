1f6f4c7b3c76625e0bfb6d0a7aca3870c737ade5
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-07-19 18:48:34 +0530

    Update form.jsx

--- client/src/form.jsx
+++ client/src/form.jsx
@@ -46,13 +46,11 @@ function Form() {
     const { image, ...dataToSend } = formData;
 
     try {
-      const response = await fetch("https://schoolidcardgenerator-1q3qnaqum-brighttnuts-projects.vercel.app/cards", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dataToSend),
-      });
+       const response = await axios.post("https://schoolidcardgenerator-1q3qnaqum-brighttnuts-projects.vercel.app/cards", dataToSend, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
 
       if (response.ok) {
         alert("Form submitted successfully");
