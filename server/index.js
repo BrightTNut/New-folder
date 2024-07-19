108e9a61f25c8b63ec5464f2fbbf00b82bdbf50f
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-07-19 18:43:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://finalschoolidcardgenerator.vercel.app/"],
+    origin: ["https://schoolid-beta.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
